65ff2746755f5529b2cc3ca8a61a38a0caf5ef76
Maria <securebean@gmail.com>
2021-03-26 12:02:20 +0300

    new dz 4 added

---
+++ kurnosova_maria_dz_4/task_4_2.py
@@ -0,0 +1,16 @@
+import requests
+from xmltodict3 import XmlTextToDict
+
+
+def currency_rates(text):
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+    doc_dict = XmlTextToDict(response.content, ignore_namespace=True).get_dict()
+    course = None
+    for i in range(0, len(doc_dict['ValCurs']['Valute'])):
+        if text.upper() in doc_dict['ValCurs']['Valute'][i]['CharCode']:
+            course = float(doc_dict['ValCurs']['Valute'][i]['Value'].replace(',', '.'))
+    return course
+
+
+print(currency_rates('USD'))
+print(currency_rates('eur'))
